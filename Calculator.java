6ff10bc7afc0133050d83ce27e2176620d30d309
Ishrat Binte Ahmed <ishratbinteahmed@gmail.com>
2025-09-21 00:40:17 +0600

    ff

--- Calculator.java
+++ Calculator.java
@@ -1,35 +1,35 @@
 public class Calculator {
-    char operator;
-    double num1,num2;
-    public Calculator(){}
-    public Calculator(char operator,double num1,double num2){
-        this.operator=operator;
-        this.num1=num1;
-        this.num2=num2;
-    }
-    public double operation(char op,double num1,double num2){
-        // edit from main 
-        // does not return anything
-        if(op=='+') return num1+num2;
-        else if(op=='-') return num1-num2;
-        else if(op=='*') return num1*num2;
-        else
-        {
-            if(num2==0)
-            {
-                System.out.println("Division by zero is not possible");
-                return 0;
-            }
-            else return num1/num2;
-        }
-        // return result;
+
+    public Calculator() {}
+
+    // Optional constructor if you want to store numbers and operator
+    public Calculator(char operator, double num1, double num2) {
+        // Not used in this example
     }
 
-    public static void main(String[] args){
-        Calculator calculator=new Calculator();
-        System.out.println(calculator.operation('+',5,3));
-        System.out.println(calculator.operation('-',5,3));  
-        System.out.println(calculator.operation('*',5,3));
-        System.out.println(calculator.operation('/',5,0));
+    public double operation(char op, double num1, double num2) {
+        switch(op) {
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2;
+            case '/':
+                if(num2 == 0) {
+                    System.out.println("Division by zero is not possible");
+                    return 0; // could also throw an exception
+                }
+                return num1 / num2;
+            default:
+                System.out.println("Invalid operator");
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.operation('+', 5, 3));  // 8.0
+        System.out.println(calculator.operation('-', 5, 3));  // 2.0
+        System.out.println(calculator.operation('*', 5, 3));  // 15.0
+        System.out.println(calculator.operation('/', 5, 0));  // Division by zero is not possible, prints 0.0
+        System.out.println(calculator.operation('/', 5, 2));  // 2.5
     }
 }
